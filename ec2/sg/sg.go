9ce4febc00a881bec706486fe1af344edea9bcd2
Leo Chen <leo@harmony.one>
2019-09-17 12:07:55 -0700

    [sg] support json configuratoin on the ip address
    Signed-off-by: Leo Chen <leo@harmony.one>

--- ec2/sg/sg.go
+++ ec2/sg/sg.go
@@ -6,15 +6,26 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 
-	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strconv"
 	"strings"
 )
+
+// IPConfig is a struct of Ip configuration
+type IPConfig struct {
+	IP       string `json:"ip"`
+	Port     int64  `json:"port"`
+	Protocol string `json:"protocol"`
+	Desc     string `json:"desc"`
+}
+
+// IPConfigs is a struct with a list of Ip configuration
+type IPConfigs struct {
+	Configs []*IPConfig `json:"ingress"`
+}
 
 // AwsConfig is the struct to parse the aws.json
 type AwsConfig struct {
@@ -26,6 +37,22 @@ type AwsConfig struct {
 func exitErrorf(msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
 	os.Exit(1)
+}
+
+func parseIPFile(filename string) (*IPConfigs, error) {
+	conf := new(IPConfigs)
+
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read file: %v", filename)
+	}
+
+	err = json.Unmarshal([]byte(content), conf)
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshal json: %v", filename)
+	}
+
+	return conf, nil
 }
 
 func parseConfig(filename string) (map[string]AwsConfig, error) {
@@ -44,48 +71,25 @@ func parseConfig(filename string) (map[string]AwsConfig, error) {
 	return conf, nil
 }
 
-func parseIPFile(filename string) (map[string]string, error) {
-	var ips = make(map[string]string)
-
-	file, err := os.Open(filename)
-	if err != nil {
-		return ips, err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		items := strings.Split(line, ",")
-		ips[items[0]] = items[1]
-	}
-
-	if err := scanner.Err(); err != nil {
-		return ips, err
-	}
-
-	return ips, nil
-}
-
-func addIngress(filename string, svc *ec2.EC2, id string, port int64) {
+func addIngress(filename string, svc *ec2.EC2, id string) {
 	ips, err := parseIPFile(filename)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "parse IP address error:", err)
 		return
 	}
 
-	for ip, name := range ips {
+	for _, ipconfig := range ips.Configs {
 		_, err := svc.AuthorizeSecurityGroupIngress(&ec2.AuthorizeSecurityGroupIngressInput{
 			GroupId: aws.String(id),
 			IpPermissions: []*ec2.IpPermission{
 				(&ec2.IpPermission{}).
-					SetIpProtocol("tcp").
-					SetFromPort(port).
-					SetToPort(port).
+					SetIpProtocol(ipconfig.Protocol).
+					SetFromPort(ipconfig.Port).
+					SetToPort(ipconfig.Port).
 					SetIpRanges([]*ec2.IpRange{
 						{
-							CidrIp:      aws.String(ip),
-							Description: aws.String(name),
+							CidrIp:      aws.String(ipconfig.IP),
+							Description: aws.String(ipconfig.Desc),
 						},
 					}),
 			},
@@ -94,13 +98,13 @@ func addIngress(filename string, svc *ec2.EC2, id string, port int64) {
 			if aerr, ok := err.(awserr.Error); ok {
 				switch aerr.Code() {
 				case "InvalidPermission.Duplicate":
-					fmt.Fprintln(os.Stderr, "Ignore Duplicated Ingress Rule: ", ip)
+					fmt.Fprintln(os.Stderr, "Ignore Duplicated Ingress Rule: ", ipconfig.IP)
 				default:
-					exitErrorf("Unable to set security group %q ingress, (%v) %v", name, aerr.Code(), err)
+					exitErrorf("Unable to set security group %q ingress, (%v) %v", ipconfig.Desc, aerr.Code(), err)
 				}
 			}
 		} else {
-			fmt.Fprintln(os.Stderr, "Added Ingress Rule: ", ip, port)
+			fmt.Fprintln(os.Stderr, "Added Ingress Rule: ", ipconfig.IP)
 		}
 	}
 
@@ -108,8 +112,7 @@ func addIngress(filename string, svc *ec2.EC2, id string, port int64) {
 }
 
 func main() {
-	portPtr := flag.String("ports", "22", "list of open port")
-	filePtr := flag.String("file", "ips.txt", "filename of IP addresses")
+	filePtr := flag.String("file", "ips.json", "json file of IP/port configuration")
 	actionPtr := flag.String("action", "list", "actions: list, add")
 	regionPtr := flag.String("region", "pdx", "AWS region airport code. ex, pdx, iad, dub, sfo")
 	groupPtr := flag.String("sg", "", "list of security group ids")
@@ -120,7 +123,6 @@ func main() {
 	action := *actionPtr
 	region := *regionPtr
 	groupIds := strings.Split(*groupPtr, ",")
-	openPorts := strings.Split(*portPtr, ",")
 	IPFile := *filePtr
 
 	Config, err := parseConfig(*awsConfPtr)
@@ -158,14 +160,7 @@ func main() {
 		fmt.Println(result)
 	case "add":
 		for _, group := range result.SecurityGroups {
-			for _, port := range openPorts {
-				np, err := strconv.ParseInt(port, 10, 64)
-				if err != nil {
-					fmt.Errorf("parsing port error: %v", err)
-				} else {
-					addIngress(IPFile, svc, *group.GroupId, np)
-				}
-			}
+			addIngress(IPFile, svc, *group.GroupId)
 		}
 	}
 
